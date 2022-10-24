74e5b61b98a1bfc2b4b30616a0d70d6ce12009f5
shoham <shohamweiss@gmail.com>
2022-10-23 21:00:18 -0500

    remove and create diarization folder

--- Diarization.py
+++ Diarization.py
@@ -6,7 +6,8 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 from pyannote.audio import Pipeline
 from pydub import AudioSegment
-
+import os
+import shutil
 
 class Diarization():
     def __init__(self):
@@ -19,6 +20,12 @@ class Diarization():
             diarization = self.model(filename)
             song = AudioSegment.from_wav(filename)
             i = 0
+                        
+            folder = "diarization"
+            if os.path.exists(folder):
+                shutil.rmtree(folder)
+            os.mkdir(folder)
+            
             for turn, _, speaker in diarization.itertracks(yield_label=True):                
                 print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
                 song[turn.start*1000:turn.end*1000].export(f"diarization/audio_{speaker}_{i}.wav", format="wav")
